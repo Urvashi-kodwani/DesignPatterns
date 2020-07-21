25819db236877f97110a5dcae6b071ed608bfa35
unknown <kodwaniurvashi@gmail.com>
2020-07-20 21:40:46 -0700

    pushing singleton

--- src/singletondesignpattern/SingletonDesignPattern.java
+++ src/singletondesignpattern/SingletonDesignPattern.java
@@ -9,6 +9,35 @@ package singletondesignpattern;
  *
  * @author Lenovo
  */
+
+class SingleObject {
+
+   //create an object of SingleObject
+   private static SingleObject instance = new SingleObject();
+
+   //make the constructor private so that this class cannot be
+   //instantiated
+   private SingleObject(){}
+
+   //Get the only object available
+   public static SingleObject getInstance(){
+      return instance;
+   }
+
+   public void showMessage(){
+      System.out.println("Hello World!");
+   }
+}
 public class SingletonDesignPattern {
-    
+    public static void main(String[] args) {
+        //illegal construct
+      //Compile Time Error: The constructor SingleObject() is not visible
+      //SingleObject object = new SingleObject();
+
+      //Get the only object available
+      SingleObject object = SingleObject.getInstance();
+
+      //show the message
+      object.showMessage();
+    }
 }
