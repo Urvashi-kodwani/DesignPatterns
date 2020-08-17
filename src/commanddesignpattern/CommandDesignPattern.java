2c9df64a87af9f3625d6d4a82b17b20c7f152834
unknown <kodwaniurvashi@gmail.com>
2020-08-16 22:03:58 -0700

    adding command pattern

---
+++ src/commanddesignpattern/CommandDesignPattern.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commanddesignpattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+interface Order{
+    void execute();
+}
+
+class Stock {
+	
+   private String name = "ABC";
+   private int quantity = 10;
+
+   public void buy(){
+      System.out.println("Stock [ Name: "+name+", Quantity: " + quantity +" ] bought");
+   }
+   public void sell(){
+      System.out.println("Stock [ Name: "+name+", Quantity: " + quantity +" ] sold");
+   }
+}
+
+class BuyStock implements Order {
+   private Stock abcStock;
+
+   public BuyStock(Stock abcStock){
+      this.abcStock = abcStock;
+   }
+
+   public void execute() {
+      abcStock.buy();
+   }
+}
+
+class SellStock implements Order {
+   private Stock abcStock;
+
+   public SellStock(Stock abcStock){
+      this.abcStock = abcStock;
+   }
+
+   public void execute() {
+      abcStock.sell();
+   }
+}
+
+class Broker {
+   private List<Order> orderList = new ArrayList<Order>(); 
+
+   public void takeOrder(Order order){
+      orderList.add(order);		
+   }
+
+   public void placeOrders(){
+   
+      for (Order order : orderList) {
+         order.execute();
+      }
+      orderList.clear();
+   }
+}
+
+
+public class CommandDesignPattern {
+    public static void main(String[] args) {
+      Stock abcStock = new Stock();
+
+      BuyStock buyStockOrder = new BuyStock(abcStock);
+      SellStock sellStockOrder = new SellStock(abcStock);
+
+      Broker broker = new Broker();
+      broker.takeOrder(buyStockOrder);
+      broker.takeOrder(sellStockOrder);
+
+      broker.placeOrders();
+    }
+}
